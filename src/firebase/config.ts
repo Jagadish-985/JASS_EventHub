608ff4eaff43e1f392c5605bcfccf9f22832eda3
Jagadish B <jagadishb.space2tech@gmail.com>
2025-11-21 08:00:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-430115181-5946e",
+  "appId": "1:61217046716:web:14cbee9893ed21185410b9",
+  "apiKey": "AIzaSyCBE5602OUYRMTD6Zc6VzuiULtcve4I8JA",
+  "authDomain": "studio-430115181-5946e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "61217046716"
+};
